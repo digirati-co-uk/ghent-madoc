a8e432d062590af6248ab4fbf9f5a9b9c8ecba43
Donald Gray <donald.gray@digirati.com>
2020-03-17 17:51:12 +0000

    Updates after applying Gateway changes
    Added new parameterStore params.
    Hardcode APP_ENV as new docker-compose would be used for new env

--- infrastructure/parameters.tf
+++ infrastructure/parameters.tf
@@ -32,9 +32,20 @@ resource "aws_ssm_parameter" "mysql_user" {
 }
 
 resource "aws_ssm_parameter" "madoc_domain" {
-  name  = "/madoc/${var.prefix}/${terraform.workspace}/MADOC_DOMAIN"
+  name  = "/madoc/${var.prefix}/${terraform.workspace}/MAIN_SITE_DOMAIN"
   type  = "String"
   value = "http://${var.madoc_domain}"
+
+  tags = {
+      "terraform" = true,
+      "system" = "madoc"
+  }
+}
+
+resource "aws_ssm_parameter" "internal_url" {
+  name  = "/madoc/${var.prefix}/${terraform.workspace}/OMEKA_INTERNAL_URL"
+  type  = "String"
+  value = "http://madoc-platform-omeka/"
 
   tags = {
       "terraform" = true,
